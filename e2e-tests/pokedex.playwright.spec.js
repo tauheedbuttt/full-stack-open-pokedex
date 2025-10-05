25ffaa1ee2219d7409ba5d3258989829b40ffc3a
tauheedbuttt <tauheedbutt@gmail.com>
2025-10-05 08:53:18 +0300

    Add test for opening a specific Pokémon in Pokedex

--- e2e-tests/pokedex.playwright.spec.js
+++ e2e-tests/pokedex.playwright.spec.js
@@ -10,4 +10,13 @@ describe("Pokedex", () => {
       )
     ).toBeVisible();
   });
+
+  test("a specific pokemon can be opened", async ({ page }) => {
+    await page.goto("");
+    await page.getByRole("link", { name: "ivysaur" }).click();
+    await expect(
+      page.locator(".pokemon-name", { name: "ivysaur" })
+    ).toBeVisible();
+    await expect(page.getByText("chlorophyll")).toBeVisible();
+  });
 });
