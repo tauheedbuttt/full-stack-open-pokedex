cfebda59811db37a08aec0eab45891394432d604
tauheedbuttt <tauheedbutt@gmail.com>
2025-10-05 09:11:19 +0300

    Add version endpoint and serve index.html for root path

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ const PORT = process.env.PORT || 5001;
 
 app.use(express.static("dist"));
 
+app.get("/version", (req, res) => {
+  res.send("1"); // change this string to ensure a new version deployed
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(`${__dirname}/dist/index.html`);
+});
+
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`server started on port ${PORT}`);
